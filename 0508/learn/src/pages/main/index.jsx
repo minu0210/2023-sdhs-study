db8b998d5ba0d11d274c8d2d9dacc15a9539e2cd
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-09 08:53:49 +0900

    Rename: main.jsx to index.jsx

---
+++ 0508/learn/src/pages/main/index.jsx
@@ -0,0 +1,14 @@
+import Button from "../../components/Button";
+
+import * as S from "./styled";
+
+function Main() {
+  return (
+    <S.Container>
+      <div>메인페이지</div>
+      <Button>만들기</Button>
+    </S.Container>
+  );
+}
+
+export default Main;
