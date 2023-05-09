bf432694d195f9467d184f438d3654f1999d05f3
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-09 11:20:45 +0900

    Feat: CreateItemBox

---
+++ 0508/learn/src/pages/todos/CreateItemBox/index.jsx
@@ -0,0 +1,21 @@
+import Button from "../../../components/Button";
+
+import * as S from "./styled";
+
+function CreateItemBox({ onChange }) {
+  const handleInputValue = (e) => {
+    onChange(e.target.value);
+  };
+
+  return (
+    <S.InputBox>
+      <S.Input
+        placeholder="To do를 입력해주세요."
+        onChange={handleInputValue}
+      />
+      <Button>등록</Button>
+    </S.InputBox>
+  );
+}
+
+export default CreateItemBox;
