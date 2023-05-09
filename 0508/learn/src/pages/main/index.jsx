269463cdfcae1756ad39d8980b9d9dc4d1a53a40
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-09 09:22:27 +0900

    Feat: add MainWrapper

--- 0508/learn/src/pages/main/index.jsx
+++ 0508/learn/src/pages/main/index.jsx
@@ -5,8 +5,10 @@ import * as S from "./styled";
 function Main() {
   return (
     <S.Container>
-      <div>메인페이지</div>
-      <Button>만들기</Button>
+      <S.MainWrapper>
+        <div>메인페이지</div>
+        <Button>만들기</Button>
+      </S.MainWrapper>
     </S.Container>
   );
 }
