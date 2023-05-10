56f0bad4c1717e9d18b3261b709e79aaa55b3ef0
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-10 11:18:03 +0900

    Feat: Todo component edit

--- 0508/learn/src/components/Todo/index.jsx
+++ 0508/learn/src/components/Todo/index.jsx
@@ -3,11 +3,17 @@ import Button from "../Button";
 import * as S from "./styled";
 
 function Todo(props) {
+  const { deleteTodo } = props;
+
   return (
     <S.Container>
       <S.Title>{props.children}</S.Title>
-      <Button>중요</Button>
-      <Button>삭제</Button>
+      <S.ButtonWrap>
+        <Button className="important">중요</Button>
+        <Button className="delete" onClick={deleteTodo}>
+          삭제
+        </Button>
+      </S.ButtonWrap>
     </S.Container>
   );
 }
--- 0508/learn/src/components/Todo/styled.js
+++ 0508/learn/src/components/Todo/styled.js
@@ -1,6 +1,43 @@
 import styled from "styled-components";
 
-export const Container = styled.li``;
+import { Button } from "../Button/styled";
+
+export const Container = styled.li`
+  width: 90%;
+  list-style: none;
+  border-radius: 10px;
+  box-shadow: 0 0 4.5px rgba(156, 156, 156, 0.6);
+  display: flex;
+  align-items: center;
+  padding: 10px 16px;
+  margin-top: 20px;
+  justify-content: space-between;
+  }
+`;
+
+export const ButtonWrap = styled.div`
+  display: flex;
+
+> ${Button} {
+  padding: 6px 10px;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+  margin: 0 4px;
+  color: #fff;
+  &:first-child {
+    background: #ffdd8a;
+  }
+  &:last-child {
+    background: #ff968a;
+  }
+  &:first-child:hover {
+    background: #FFD36D;
+  }
+  &:last-child:hover {
+    background: #ff7d6f;
+  }
+`;
 
 export const Title = styled.p`
   font-size: 16px;
