fb040ecaab1ff6b320e933e85a844e60854b8714
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-10 11:20:26 +0900

    Feat: todos page edit

--- 0508/learn/src/pages/todos/index.jsx
+++ 0508/learn/src/pages/todos/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import * as S from "./styled";
 
@@ -10,18 +11,32 @@ function Todos() {
   const [todos, setTodos] = useState([]);
 
   const createTodo = () => {
-    setTodos((prevState) => [
-      ...prevState,
-      { id: prevState.length + 1, name: todoName },
-    ]);
+    // if (!todoName.trim()) {
+    //   alert("값이 없습니다.");
+    //   return;
+    // }
+    setTodoName("");
+    setTodos((prevState) => [...prevState, { id: uuidv4(), name: todoName }]);
+  };
+
+  const deleteTodo = (id) => {
+    const findIndex = todos.findIndex((v) => v.id === id);
+    setTodos((prevState) => {
+      const tempArr = [...prevState];
+      tempArr.splice(findIndex, 1);
+      return tempArr;
+    });
   };
 
   return (
     <S.Container>
       <S.Title>To do list</S.Title>
-      {todoName}
-      <CreateItemBox onChange={setTodoName} createTodo={createTodo} />
-      <ItemList todos={todos} />
+      <CreateItemBox
+        value={todoName}
+        onChange={setTodoName}
+        createTodo={createTodo}
+      />
+      <ItemList todos={todos} deleteTodo={deleteTodo} />
     </S.Container>
   );
 }
