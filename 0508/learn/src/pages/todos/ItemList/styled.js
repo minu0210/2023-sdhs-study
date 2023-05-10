79df0e405b2cf72b34c677b82b4cdd6f266e4b16
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-10 11:19:30 +0900

    Feat: itemList edit

--- 0508/learn/src/pages/todos/ItemList/styled.js
+++ 0508/learn/src/pages/todos/ItemList/styled.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const ItemList = styled.ul`
-    display: flex;
-    flex-direction: column;
-    gapL 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  width: 100%;
 `;
