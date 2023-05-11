e06bc04335d72b8d277d7b4f14893ad613d9a150
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-11 10:21:39 +0900

    Fix: important button delete

--- 0508/learn/src/components/Todo/index.jsx
+++ 0508/learn/src/components/Todo/index.jsx
@@ -9,7 +9,6 @@ function Todo(props) {
     <S.Container>
       <S.Title>{props.children}</S.Title>
       <S.ButtonWrap>
-        <Button className="important">중요</Button>
         <Button className="delete" onClick={deleteTodo}>
           삭제
         </Button>
