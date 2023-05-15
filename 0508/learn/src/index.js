5410869c7b1c739631016be7cbf887363d4d44cb
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-15 16:10:01 +0900

    Feat: add redux store

--- 0508/learn/src/index.js
+++ 0508/learn/src/index.js
@@ -1,8 +1,15 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import { store } from "./reduce";
 
 import Router from "./pages/Router";
 
 import "./reset.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Router />);
+root.render(
+  <Provider store={store}>
+    <Router />
+  </Provider>
+);
