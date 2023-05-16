feb64cfc60c26a6f136cd172eb3dd3544a1f697e
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-17 08:32:57 +0900

    Feat: set ItemList component

---
+++ 0516/list/src/components/Itemlist/index.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import * as S from "./styled";
+import data from "../../data";
+import Item from "../Item";
+import Button from "../Button";
+
+function ItemList() {
+  const [list, setList] = useState(data.data);
+
+  const ascendingId = () => {
+    let copy = [...data.data];
+    let result = copy.sort((a, b) => {
+      if (a.id < b.id) return -1;
+      if (a.id > b.id) return 1;
+      return 0;
+    });
+    setList(result);
+  };
+
+  const descendingId = () => {
+    let copy = [...data.data];
+    let result = copy.sort((a, b) => {
+      if (a.id < b.id) return 1;
+      if (a.id > b.id) return -1;
+      return 0;
+    });
+    setList(result);
+  };
+
+  const ascendingLevel = () => {
+    let copy = [...data.data];
+    let result = copy.sort((a, b) => {
+      if (a.level === b.level) {
+        if (a.id < b.id) return -1;
+        else return 1;
+      }
+      if (a.level === undefined) return -1;
+      if (a.level < b.level) return 1;
+      if (a.level > b.level) return -1;
+      return 0;
+    });
+    setList(result);
+  };
+
+  const ascendingName = () => {
+    let copy = [...data.data];
+    let result = copy.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+    setList(result);
+  };
+
+  const descendingName = () => {
+    let copy = [...data.data];
+    let result = copy.sort((a, b) => {
+      if (a.name < b.name) return 1;
+      if (a.name > b.name) return -1;
+      return 0;
+    });
+    setList(result);
+  };
+
+  return (
+    <>
+      <S.ButtonWrap>
+        <Button onClick={ascendingId}>ID 오름차순</Button>
+        <Button onClick={descendingId}>ID 내림차순</Button>
+        <Button onClick={ascendingLevel}>LEVEL 오름차순</Button>
+        <Button onClick={ascendingName}>NAME 오름차순</Button>
+        <Button onClick={descendingName}>NAME 내림차순</Button>
+      </S.ButtonWrap>
+      <S.ListWrap>
+        <Item id="ID" name="NAME" level="LEVEL"></Item>
+        <S.Line />
+        {list.map(({ id, name, level }) => {
+          return <Item key={id} id={id} name={name} level={level}></Item>;
+        })}
+      </S.ListWrap>
+    </>
+  );
+}
+
+export default ItemList;
---
+++ 0516/list/src/components/Itemlist/styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ItemList = styled.div`
+  list-style: none;
+  padding: 4px 12px;
+  text-align: center;
+`;
+
+export const ButtonWrap = styled.div`
+  display: flex;
+  margin: 20px 0;
+  width: 600px;
+  justify-content: space-around;
+`;
+
+export const Line = styled.hr`
+  width: 300px;
+`;
+
+export const ListWrap = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
