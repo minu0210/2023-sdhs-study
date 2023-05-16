386df69ed8fcbefc9147ab79a40498072c2246b7
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-17 08:32:17 +0900

    Feat: set button component

---
+++ 0516/list/src/components/Button/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border: none;
+  cursor: pointer;
+  padding: 6px 8px;
+  border-radius: 10px;
+  &:hover {
+    background: #d6d6d6;
+  }
+  &:disabled {
+    cursor: not-allowed;
+  }
+  &:disabled:hover {
+    background: rgba(239, 239, 239, 0.3);
+  }
+`;
