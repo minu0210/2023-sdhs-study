c561142beb6bd5eb6f10e349bec4b0babf932a78
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-22 08:49:20 +0900

    TEST: test textcontent

--- 0519/login/src/pages/login/index.jsx
+++ 0519/login/src/pages/login/index.jsx
@@ -13,14 +13,12 @@ function Login({ props, name, id, password }) {
 
     const navigate = useNavigate();
 
-    const notice = document.querySelector("p");
-
-    const checkName = (e) => {
-        console.log(e.target.value);
-        const regExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
-        if (regExp.test(e.target.value)) console.log("특수문자는 사용할 수 없습니다.");
-        // else notice.textContent = " ";
-        if (e.target.value.length < 2 || e.target.value.length > 5) notice.textContent = "이름은 2글자 이상 5글자 이하만 입력할 수 있습니다.";
+    const checkName = ({ name }) => {
+        console.log(name);
+        // const regExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
+        // if (regExp.test(name.target.value)) alert("특수문자는 사용할 수 없습니다.");
+        // // else notice.textContent = " ";
+        // if (name.target.value.length < 2 || name.target.value.length > 5) alert("이름은 2글자 이상 5글자 이하만 입력할 수 있습니다.");
     };
 
     const checkId = (e) => {};
@@ -33,13 +31,10 @@ function Login({ props, name, id, password }) {
     return (
         <S.Container>
             <S.Title>로그인</S.Title>
-            <Input type="text" placeholder="이름" onChange={checkName} value={name}></Input>
-            <S.Notice>{props}</S.Notice>
-            <Input type="text" placeholder="아이디" onChange={checkId} value={id}></Input>
-
-            <Input type="password" placeholder="비밀번호" onChange={checkPw} value={password}></Input>
-
-            <Button onClick={loadMain}>로그인</Button>
+            <Input type="text" placeholder="이름" value={name}></Input>
+            <Input type="text" placeholder="아이디" value={id}></Input>
+            <Input type="password" placeholder="비밀번호" value={password}></Input>
+            <Button onClick={checkName}>로그인</Button>
         </S.Container>
     );
 }
