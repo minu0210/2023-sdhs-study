a1bec11b2f556c232b7265ffee50c84fae996ae6
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-23 13:24:09 +0900

    Fix: rename folder

--- 0522/todo-width-js/src/types/todo.ts
+++
@@ -1,6 +0,0 @@
-interface Todo {
-    id: string;
-    name: string;
-}
-
-export type { Todo };
--- 0522/todo-width-js/src/types/util.ts
+++
@@ -1,5 +0,0 @@
-import type { Dispatch, SetStateAction } from "react";
-
-type DispatchSetStateAction<T> = Dispatch<SetStateAction<T>>;
-
-export type { DispatchSetStateAction };
