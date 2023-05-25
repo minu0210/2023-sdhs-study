a7c1bc897529bde456a5cfed9e167abd0637ce41
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-25 15:12:43 +0900

    FEAT: Input component

--- 0522/todo-with-ts/src/components/Input/index.tsx
+++ 0522/todo-with-ts/src/components/Input/index.tsx
@@ -1,43 +1,8 @@
 import { InputHTMLAttributes } from "react";
-// // 1번째 type과 interface 차이
+import * as S from "./styled";
 
-// // 1 - type: 단일 형식, 오브젝트 형식 모두 가능
-// type Color = string; // 단일 형식
-// type MyType = { color: Color /* =string */ }; // 오브젝트 형식
-
-// // 2 - interface: 오브젝트 형식만 가능
-// interface MyInterface { // 오브젝트 형식만 가능
-//     color: string;
-// }
-// const MyData2: MyInterface = { color: "red" };
-
-// 2번째 type과 interface 차이
-// type: |(or) &(and) 연산자를 사용 할 수 있음
-type OpacityData = { opacity: number };
-type MyType = { color: string };
-// | > or => 둘 중 하나만 들어가도 됨
-// & > and => 둘 다 반드시 들어가야 함
-type MyMixType = MyType | OpacityData;
-// MyType 또는 OpacityData
-const data: MyMixType = { color: "red", opacity: 1 };
-
-// interface: 부모 인터페이스로부터 extends(상속)을 사용할 수 있음
-interface MyOpacity {
-    opacity: number;
-}
-interface MyInterface extends MyOpacity {
-    color: string;
-}
-
-const data2: MyInterface = { color: "red", opacity: 1 };
-
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    color?: string; // (= | undefined);
-}
-
-function Input(props: InputProps) {
-    // props.color = 'red';
-    return <input {...props} />;
+function Input(props: InputHTMLAttributes<HTMLInputElement>) {
+    return <S.Input {...props} />;
 }
 
 export default Input;
---
+++ 0522/todo-with-ts/src/components/Input/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+    padding: 6px 12px;
+    border-radius: 20px;
+    border: none;
+    box-shadow: 0 0 0 3px rgba(172, 172, 172, 0.5);
+    &:focus {
+        outline: none;
+    }
+`;
