556e002f67a851700e39dad4f8b7192a2cfcaca5
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-25 15:12:31 +0900

    FEAT: Todo component

--- 0522/todo-with-ts/src/components/Todo/index.tsx
+++ 0522/todo-with-ts/src/components/Todo/index.tsx
@@ -1,19 +1,42 @@
 import type { Todo as TodoType } from "../../types/todo";
+import type { ChangeEvent, KeyboardEvent } from "react";
+
+import * as S from "./styled";
 
 import Button from "../Button";
+import Input from "../Input";
 
 interface TodoProps {
     name: TodoType["name"];
+    isEdited: boolean;
     deleteTodo: () => void;
     handleSelected: (checked: boolean) => void;
+    onClick: () => void;
+    toggleEditTodo: () => void;
+    setEditedName: (name: TodoType["name"]) => void;
+    editTodo: () => void;
 }
 
-function Todo({ name, deleteTodo, handleSelected }: TodoProps) {
+function Todo({ name, deleteTodo, onClick, toggleEditTodo, isEdited, setEditedName, editTodo }: TodoProps) {
+    const handleEditedName = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+        setEditedName(value);
+    };
+
+    const handleEditEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") editTodo();
+    };
+
     return (
-        <li>
-            <span>{name}</span>
-            <Button onClick={deleteTodo}>삭제</Button>
-        </li>
+        <S.Wrap>
+            <S.List onClick={onClick}>
+                {isEdited ? <Input onKeyUp={handleEditEnter} defaultValue={name} onChange={handleEditedName} autoFocus /> : <span>{name}</span>}
+            </S.List>
+            <S.ButtonWrap>
+                <Button onClick={deleteTodo}>삭제</Button>
+                <Button onClick={toggleEditTodo}>{isEdited ? "취소" : "수정"}</Button>
+                {isEdited && <Button onClick={editTodo}>저장</Button>}
+            </S.ButtonWrap>
+        </S.Wrap>
     );
 }
 
---
+++ 0522/todo-with-ts/src/components/Todo/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const List = styled.li`
+    cursor: pointer;
+    list-style: none;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    width: 400px;
+    padding: 10px 24px;
+    align-items: center;
+    font-family: "omyu_pretty";
+`;
+
+export const Wrap = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ButtonWrap = styled.div``;
