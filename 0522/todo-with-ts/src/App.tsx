bc284f8e697bc3ad431e9eccb8c543e947c45066
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-25 15:14:03 +0900

    FEAT: create App page

--- 0522/todo-with-ts/src/App.tsx
+++ 0522/todo-with-ts/src/App.tsx
@@ -1,17 +1,22 @@
-import "./App.css";
-import { ChangeEvent, useEffect, useState } from "react";
+import "./reset.css";
+import * as S from "./styled";
+import { ChangeEvent, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import Input from "./components/Input";
+import Button from "./components/Button";
 import TodoList from "./components/TodoList";
 
 import type { Todo } from "./types/todo";
+import type { KeyboardEvent } from "react";
 
 function App() {
     const [todoName, setTodoName] = useState<Todo["name"]>("");
     const [todos, setTodos] = useState<Todo[]>([]);
     const [searchValue, setSearchValue] = useState<string>("");
     const [selectedTodoIds, setSelectedTodoIds] = useState<Todo["id"][]>([]);
+    const [editedTodoId, setEditedTodoId] = useState<Todo["id"] | undefined>();
+    const [editedName, setEditedName] = useState<Todo["name"]>("");
 
     const handleTodoName = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
         setTodoName(value);
@@ -30,17 +35,55 @@ function App() {
 
     const addTodo = () => {
         setTodos((prevState) => [...prevState, { id: uuidv4(), name: todoName }]);
-
         setTodoName("");
     };
 
+    const handleEnterPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter" && todoName.trim()) addTodo();
+    };
+
+    const toggleEditTodo = (id: Todo["id"]) => {
+        setEditedTodoId((prevState) => (prevState === id ? undefined : id));
+    };
+
+    const handleEditedName = (name: Todo["name"]) => {
+        setEditedName(name);
+    };
+
+    const editTodo = () => {
+        setTodos((prevState) => {
+            return prevState.map(({ id, name }) => {
+                if (id === editedTodoId) {
+                    return { id, name: editedName };
+                }
+                return { id, name };
+            });
+        });
+        setEditedTodoId(undefined);
+        setEditedName("");
+    };
+
     return (
-        <div className="App">
-            <Input value={todoName} onChange={handleTodoName} />
-            <button onClick={addTodo}>Todo 추가</button>
-            <Input onChange={handleSearchValue} />
-            <TodoList todos={todos} searchValue={searchValue} deleteTodo={handleDeleteTodo} setSelectedTodoIds={setSelectedTodoIds} />
-        </div>
+        <S.Container>
+            <S.Wrap>
+                <Input placeholder="할 일을 입력해주세요" value={todoName} onKeyUp={handleEnterPress} onChange={handleTodoName} />
+                <Button onClick={addTodo} disabled={!todoName.trim()}>
+                    Todo 추가
+                </Button>
+            </S.Wrap>
+            <Input placeholder="검색할 내용을 입력해주세요" onChange={handleSearchValue} />
+            <TodoList
+                todos={todos}
+                searchValue={searchValue}
+                deleteTodo={handleDeleteTodo}
+                setSelectedTodoIds={setSelectedTodoIds}
+                selectedTodoIds={selectedTodoIds}
+                editedTodoId={editedTodoId}
+                toggleEditTodo={toggleEditTodo}
+                setEditedName={handleEditedName}
+                editTodo={editTodo}
+            />
+        </S.Container>
     );
 }
 
---
+++ 0522/todo-with-ts/src/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    text-align: center;
+    padding: 100px;
+`;
+
+export const Wrap = styled.div`
+    margin-bottom: 40px;
+`;
