cb0ccad53ba76fb82ae718e510ef9f8db64d3958
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-05-25 15:11:38 +0900

    STYLE: prettier style edit

--- 0508/learn/src/components/Checkbox/index.jsx
+++ 0508/learn/src/components/Checkbox/index.jsx
@@ -1,11 +1,11 @@
 import * as S from "./styled";
 
 function Checkbox({ onChange }) {
-  const handleChecked = (e) => {
-    onChange(e.target.checked);
-  };
+    const handleChecked = (e) => {
+        onChange(e.target.checked);
+    };
 
-  return <S.Checkbox type="checkbox" onChange={handleChecked} />;
+    return <S.Checkbox type="checkbox" onChange={handleChecked} />;
 }
 
 export default Checkbox;
